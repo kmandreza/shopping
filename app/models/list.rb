95a4581d85f7fc2c477ee674b707311e5576d75f
Khara Muniz <kmandreza@gmail.com>
2014-02-15 16:42:24 -0800

    shopping app

---
+++ app/models/list.rb
@@ -0,0 +1,3 @@
+class List < ActiveRecord::Base
+  attr_accessible :descrption, :item, :qty, :store
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopping::Application.config.secret_token = 'c681b17f608e4b680a36620109eb3155d4a06d2d1d5142182895f14807e40069fb1bd0b177239a9cb601015335ea2d283e82d289c9d704bd1b8493cecc9397d5'
